16f8759631a26c24f145f4f37fbf2b68acb1e173
Mohammed Rahman <minhaz357@gmail.com>
2022-04-11 00:50:17 -0400

    Retrieve all items from Parse backend
    	- Test to check if the app can connect to the Parse backend

--- app/src/main/java/com/shoppit/activities/MainActivity.java
+++ app/src/main/java/com/shoppit/activities/MainActivity.java
@@ -1,8 +1,17 @@
-package com.shoppit;
+package com.shoppit.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.shoppit.R;
+import com.shoppit.models.Item;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get items from the Parse backend
+        queryItem();
+    }
+
+    private void queryItem() {
+        // Define the class we would like to query
+        ParseQuery<Item> itemParseQuery = ParseQuery.getQuery(Item.class);
+
+        // Include user key
+        itemParseQuery.include(Item.KEY_ITEM_CATEGORY);
+        // Execute the find asynchronously
+        itemParseQuery.findInBackground(new FindCallback<Item>() {
+            @Override
+            public void done(List<Item> items, ParseException e) {
+                if (e != null){
+                    Log.e("MainActivity", "ERROR while retrieving items", e);
+                    return;
+                }
+
+                for (Item item: items){
+                    Log.i("MainActivity", "Item: " + item.getItemName() + " Category Name: " + item.getCategory().getCategoryName());
+                }
+            }
+        });
     }
 }
