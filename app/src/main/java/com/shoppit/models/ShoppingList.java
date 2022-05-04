f9b4c86dfd2d5038bab864aacf0e77c56d2315e8
Mohammed Rahman <minhaz357@gmail.com>
2022-05-04 18:47:01 -0400

    Update model ShoppingList
    	- Add keys
    	- Create getters and setters

--- app/src/main/java/com/shoppit/models/ShoppingList.java
+++ app/src/main/java/com/shoppit/models/ShoppingList.java
@@ -1,4 +1,36 @@
 package com.shoppit.models;
 
-public class ShoppingList {
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import org.parceler.Parcel;
+
+@ParseClassName("ShoppingList")
+@Parcel(analyze={ShoppingList.class})
+public class ShoppingList extends ParseObject {
+
+    public static final String KEY_SHOPPING_LIST_NAME = "shoppingListName";
+    public static final String KEY_USER = "user";
+
+    // empty constructor needed by the Parceler library
+    public ShoppingList(){
+    }
+
+    public String getShoppingListName(){
+        return getString(KEY_SHOPPING_LIST_NAME);
+    }
+
+    public void setShoppingListName(String shoppingListName){
+        put(KEY_SHOPPING_LIST_NAME, shoppingListName);
+    }
+
+    public ParseUser getUser(){
+        return getParseUser(KEY_USER);
+    }
+
+    public void setUser(ParseUser user){
+        put(KEY_USER, user);
+    }
+
 }
