f4d1f03dc75d40291abe3df47f2f244a116719f7
Mohammed Rahman <minhaz357@gmail.com>
2022-05-05 01:31:51 -0400

    Create add and remove buttons to add/remove items to cart

--- app/src/main/java/com/shoppit/adapters/SearchItemsAdapter.java
+++ app/src/main/java/com/shoppit/adapters/SearchItemsAdapter.java
@@ -23,6 +23,7 @@ public class SearchItemsAdapter extends RecyclerView.Adapter<SearchItemsAdapter.
     Context context;
     List<Item> items;
     List<Item> allItems;
+    int itemCount;
 
     public SearchItemsAdapter(Context context, List<Item> items) {
         this.context = context;
@@ -59,12 +60,20 @@ public class SearchItemsAdapter extends RecyclerView.Adapter<SearchItemsAdapter.
         TextView tvItemName;
         TextView tvItemUnitPrice;
 
+        ImageView ivRemoveItem;
+        TextView tvItemCount;
+        ImageView ivAddItem;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             ivItemImage = itemView.findViewById(R.id.ivItemImage);
             tvItemName = itemView.findViewById(R.id.tvItemName);
             tvItemUnitPrice = itemView.findViewById(R.id.tvItemUnitPrice);
+
+            ivRemoveItem = itemView.findViewById(R.id.ivRemoveItem);
+            tvItemCount = itemView.findViewById(R.id.tvItemCount);
+            ivAddItem = itemView.findViewById(R.id.ivAddItem);
 
         }
 
@@ -78,6 +87,53 @@ public class SearchItemsAdapter extends RecyclerView.Adapter<SearchItemsAdapter.
             if (image != null){
                 Glide.with(context).load(image.getUrl()).into(ivItemImage);
             }
+
+            // Set click listener to Add item image view
+            ivAddItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Get the item count from the text field
+                    itemCount = Integer.parseInt(tvItemCount.getText().toString());
+
+                    // Make the remove item button and item count field visible
+                    ivRemoveItem.setVisibility(View.VISIBLE);
+                    tvItemCount.setVisibility(View.VISIBLE);
+
+                    // Increment the item count field
+                    itemCount++;
+
+                    // Set the item count field with the new value
+                    tvItemCount.setText(String.valueOf(itemCount));
+                }
+            });
+
+            // Set click listener to Remove item image view
+            ivRemoveItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Get the item count from the text field
+                    itemCount = Integer.parseInt(tvItemCount.getText().toString());
+
+                    // decrement item count
+                    itemCount--;
+
+                    if (itemCount > 0){
+
+                        ivRemoveItem.setVisibility(View.VISIBLE);
+                        tvItemCount.setVisibility(View.VISIBLE);
+
+                        tvItemCount.setText(String.valueOf(itemCount));
+                    }
+
+                    if (itemCount == 0){
+
+                        tvItemCount.setText(String.valueOf(itemCount));
+
+                        ivRemoveItem.setVisibility(View.GONE);
+                        tvItemCount.setVisibility(View.GONE);
+                    }
+                }
+            });
         }
     }
 
