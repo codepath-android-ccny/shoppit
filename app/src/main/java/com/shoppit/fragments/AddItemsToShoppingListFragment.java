fb2af64e47148048e6c0964dda025a20180e7e56
Mohammed Rahman <minhaz357@gmail.com>
2022-05-24 06:52:34 -0400

    Create fragment to add items to shopping list

---
+++ app/src/main/java/com/shoppit/fragments/AddItemsToShoppingListFragment.java
@@ -0,0 +1,57 @@
+package com.shoppit.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.shoppit.R;
+
+public class AddItemsToShoppingListFragment extends Fragment {
+
+    // Required empty public constructor
+    public AddItemsToShoppingListFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_items_to_shopping_list, container, false);
+    }
+
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Set the title of the toolbar to null
+        Toolbar toolbar= Toolbar.class.cast(getActivity().findViewById(R.id.toolbar));
+        toolbar.setTitle(null);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        // Remove all the old menu items
+        menu.clear();
+    }
+}
