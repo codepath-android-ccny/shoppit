7c24bd285a5e8c3ac431a6c18a020d8a177cce10
Mohammed Rahman <minhaz357@gmail.com>
2022-05-24 13:05:44 -0400

    Display suggested items

--- app/src/main/java/com/shoppit/fragments/AddItemsToShoppingListFragment.java
+++ app/src/main/java/com/shoppit/fragments/AddItemsToShoppingListFragment.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,9 +17,23 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.shoppit.R;
+import com.shoppit.adapters.SearchItemsAdapter;
+import com.shoppit.adapters.SuggestedItemsAdapter;
+import com.shoppit.models.Item;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddItemsToShoppingListFragment extends Fragment {
+    private static final String TAG = "AddItemsToShoppingList";
+
+    RecyclerView rvSuggestedItems;
+    SuggestedItemsAdapter suggestedItemsAdapter;
+    List<Item> allItems;
 
     // Required empty public constructor
     public AddItemsToShoppingListFragment() {
@@ -46,6 +64,31 @@ public class AddItemsToShoppingListFragment extends Fragment {
         Toolbar toolbar= (Toolbar) requireActivity().findViewById(R.id.toolbar);
         assert toolbar != null;
         toolbar.setTitle("Add Items");
+
+        // Setup any handles to view objects here
+        rvSuggestedItems = view.findViewById(R.id.rvSuggestedItems);
+
+        // Initialize the list
+        allItems = new ArrayList<>();
+        // Initialize the adapter
+        suggestedItemsAdapter = new SuggestedItemsAdapter(getContext(), allItems);
+        // Initialize the layout
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+
+        // Decorate the RecyclerView
+        // This decorator displays dividers between each item within the list
+        RecyclerView.ItemDecoration itemDecoration = new
+                DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL);
+
+        // Set the Adapter for the RecyclerView
+        rvSuggestedItems.setAdapter(suggestedItemsAdapter);
+
+        // Set the LayoutManager for the RecyclerView
+        rvSuggestedItems.setLayoutManager(linearLayoutManager);
+        rvSuggestedItems.addItemDecoration(itemDecoration);
+
+        // Retrieve all suggested items
+        querySuggestedItems();
     }
 
     @Override
@@ -56,5 +99,31 @@ public class AddItemsToShoppingListFragment extends Fragment {
         menu.clear();
         // Inflate the new menu options
         inflater.inflate(R.menu.add_items_to_shopping_list_menu, menu);
+    }
+
+    private void querySuggestedItems() {
+        // Define the class we would like to query
+        ParseQuery<Item> itemParseQuery = ParseQuery.getQuery(Item.class);
+
+        // Include user key
+        itemParseQuery.include(Item.KEY_ITEM_CATEGORY);
+        // sort the items by name
+        itemParseQuery.orderByAscending(Item.KEY_ITEM_NAME);
+
+        // Execute the find asynchronously
+        itemParseQuery.findInBackground(new FindCallback<Item>() {
+            @Override
+            public void done(List<Item> items, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "ERROR while retrieving items", e);
+                    return;
+                }
+
+                // Add all categories to the list
+                allItems.addAll(items);
+                // Notify the adapter about the data change
+                suggestedItemsAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
